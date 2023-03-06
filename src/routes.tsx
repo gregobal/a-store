5904de9d687f3cceb3ebe13210486e819b69dff0
Gregory Balabanov <810411@mail.ru>
2023-03-06 16:24:46 +0500

    feat: policy page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import {getMadeInAlfa, getYourDesign} from "./api";
-import {contactUs, madeInAlfa, ownDesign, product} from "./constants/routes";
+import {contactUs, madeInAlfa, ownDesign, policy, product} from "./constants/routes";
 import {RootLayout} from "./layouts/root-layout";
 import {ContactPage} from "./pages/contact";
 import {ErrorPage} from "./pages/error";
@@ -8,6 +8,7 @@ import {MadeInAlfaPage} from "./pages/made-in-alfa";
 import {MainPage} from "./pages/main-page";
 import {NoMatchPage} from "./pages/no-match";
 import {OwnDesignPage} from "./pages/own-design";
+import {PolicyPage} from "./pages/policy";
 import {ProductPage} from "./pages/product";
 import {productLoader} from "./pages/product/loader";
 
@@ -45,6 +46,10 @@ export const router = createBrowserRouter([
                     {
                         path: contactUs.path,
                         element: <ContactPage/>,
+                    },
+                    {
+                        path: policy.path,
+                        element: <PolicyPage/>,
                     },
                     {
                         path: "*",
